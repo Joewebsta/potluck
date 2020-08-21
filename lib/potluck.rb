1a36708fa43a1c62132ba6716a7586067c404aad
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-21 14:07:22 -0400

    Create Potluck#ratio

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -21,23 +21,8 @@ class Potluck
       menu[dish.category] = all_from_category(dish.category)
     end
   end
-end
 
-require './lib/dish'
-potluck = Potluck.new('7-13-18')
-couscous_salad = Dish.new('Couscous Salad', :appetizer)
-summer_pizza = Dish.new('Summer Pizza', :appetizer)
-roast_pork = Dish.new('Roast Pork', :entre)
-cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
-candy_salad = Dish.new('Candy Salad', :dessert)
-bean_dip = Dish.new('Bean Dip', :appetizer)
-potluck.add_dish(couscous_salad)
-potluck.add_dish(summer_pizza)
-potluck.add_dish(roast_pork)
-potluck.add_dish(cocktail_meatballs)
-potluck.add_dish(candy_salad)
-potluck.add_dish(bean_dip)
-# pp potluck.all_from_category(:entre)
-pp potluck.menu
-# => {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]}
-# potluck.ratio(:appetizer)
+  def ratio(category)
+    (all_from_category(category).count / dishes.count.to_f * 100).round(2)
+  end
+end
